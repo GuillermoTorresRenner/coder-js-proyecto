dfc27e43da8e5c41298f145db600b903cb1475bf
LeBateleur <torresrennerguillermo@gmail.com>
2023-09-09 01:58:56 -0300

    Se arregla un problema de recuperacion del local storage

--- js/misPersonajes.js
+++ js/misPersonajes.js
@@ -3,13 +3,12 @@ const listaPersonajes = JSON.parse(localStorage.getItem("listaPjs"));
 
 listaPersonajes &&
   listaPersonajes.forEach((pj) => {
-    console.log("Soy el console log que buscabas", pj);
     misPersonajes.innerHTML += `
 <div class="card mb-3" >
   <div class="card-body">
-  <i class="fa fa-lg fa-user "></i>
+  <i class="fa fa-lg fa-user "></i> 
+  <div>ID=${pj.id} </div>
     <h5 class="card-title text-center">${pj.nombre}</h5>
-   
     <p class="card-text text-muted">${pj.descripcion}</p>
     <div class="row justify-content-center">
 <a href='./pages/editarPJ.html?id=${pj.id}' id="editar" class="btn btn-warning col-2 mx-2"><i class="fa fa-eye"></i></a>  
